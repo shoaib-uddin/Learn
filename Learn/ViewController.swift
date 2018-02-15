a42fc9a4c4643aa58c0a5923b4e95223feff82d2
Xtreme Hardware <muhammad.n@ikotel.ca>
2018-02-15 14:49:58 +0500

    Initial Commit

---
+++ Learn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Learn
+//
+//  Created by Xtreme Hardware on 15/02/2018.
+//  Copyright © 2018 pixel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
