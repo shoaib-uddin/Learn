d2dd2820dd58f636bd9caf20cceb60d4f2919cd3
Xtreme Hardware <muhammad.n@ikotel.ca>
2018-04-05 14:26:21 +0500

    HomeVC share layouts in progress

---
+++ Learn/UIViews/Cells/CollectionViewCells/TableSearchCVC.swift
@@ -0,0 +1,18 @@
+//
+//  TableSearchCVC.swift
+//  Learn
+//
+//  Created by Xtreme Hardware on 04/04/2018.
+//  Copyright © 2018 pixel. All rights reserved.
+//
+
+import UIKit
+
+class TableSearchCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Learn/ViewControllers/SidemenuVC/SearchCatSideVC.swift
@@ -0,0 +1,9 @@
+//
+//  SearchCatSideVC.swift
+//  Learn
+//
+//  Created by Xtreme Hardware on 04/04/2018.
+//  Copyright © 2018 pixel. All rights reserved.
+//
+
+import Foundation
